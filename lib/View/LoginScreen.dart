ae852c54e90d49d42f33f7475fda296c851d70c4
Moazzam Khan <moazzamk12319@gmail.com>
2025-10-20 10:08:30 -0700

    LOGINSCREEN/SPLAHSCREEN (MOAZZAM)

---
+++ lib/View/LoginScreen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {return Scaffold(
+    backgroundColor: const Color(0xFF4169E1), // Royal Blue background
+    body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2),
+            // Title Text
+            const Text(
+              "WELCOME TO SHIGOTO",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 50),
+
+            // White rounded container
+            Container(
+              width: 380,
+              height: 500,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(40),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              //Text Fields
+              child: Padding(padding: EdgeInsets.all(35.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Spacer(),
+                  TextField(
+                      decoration:InputDecoration(
+                        labelText: "Username",
+                        labelStyle: TextStyle(color: Colors.blueGrey,fontSize: 20),
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      )
+                  ),
+                  SizedBox(height: 25,),
+                  TextField(
+                      decoration:InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(color: Colors.blueGrey,fontSize: 20),
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      )
+                  ),
+                  SizedBox(height: 25,),
+
+                  SizedBox(
+                    width: 300,
+                    height: 50,
+                    child:
+                    ElevatedButton(onPressed: (){
+                    },
+                      child: Text("Login",
+                        style:TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ) ,),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:  Color(0xFF4169E1),
+                      ),),
+                  ),
+                  SizedBox(height: 25,),
+                  TextButton(onPressed: (){}, child: Text("Sign up",
+                  style:TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueGrey,
+                  ),)),
+                  Text("Powered by Flutter",style:
+                    TextStyle(fontSize: 15,color: Colors.black26),),
+                  Spacer(),
+                ],
+              ),)
+              ),
+            ],
+        ),
+        ),
+      ),
+  );
+
+  }
+}
---
+++ lib/View/SplashScreen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'LoginScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0.9, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    Timer(const Duration(seconds: 3), () {
+      /*Navigator.pushReplacementNamed(context, '/login');
+       */
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Loginscreen()),
+      );
+
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF4169E1),
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: Text(
+            'SHIGOTO',
+            style: TextStyle(
+              fontSize: 55,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              letterSpacing: 5,
+              fontFamily: 'ArialRoundedMTBold',
+              shadows: [
+                Shadow(
+                  blurRadius: 10,
+                  color: Colors.white.withOpacity(0.4),
+                  offset: const Offset(0, 0),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
